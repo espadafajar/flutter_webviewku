c17a8092a8b8f5fbb39e6d7312c17f1f16d4d0f4
Fajar Pratama <esfajarx2@gmail.com>
2023-07-07 07:38:06 +0700

    initiate

---
+++ lib/features/main/bloc/main_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'main_event.dart';
+part 'main_state.dart';
+
+class MainBloc extends Bloc<MainEvent, MainState> {
+  MainBloc() : super(MainInitial());
+
+  @override
+  Stream<MainState> mapEventToState(
+    MainEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
